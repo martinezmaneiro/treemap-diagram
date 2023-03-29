3eb033d33a0e42619dca87167ebd0e16113acb56
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-29 10:32:16 -0300

    [ADDED] tree tiles

--- index.js
+++ index.js
@@ -5,14 +5,30 @@ let movieData;
 //svg element selector
 let canvas = d3.select('#canvas');
 
+//creates a treemap once we have defined the data
 let drawTreeMap =()=> {
+    /*create a hierarchy structure using d3.hierarchy() method
+    (https://github.com/d3/d3-hierarchy#hierarchy) */
     let hierarchy = d3.hierarchy(movieData, (node) => {
         return node['children']
     }).sum((node) => {
         return node['va;ie']
     }).sort((node1, node2) => {
         return node2['value'] - node1['value']
-    })
+    });
+    /*call the .treemap() method to set the dimensions for the
+    rectangles*/
+    let createTreeMap = d3.treemap()
+                            .size([1000, 600]);
+    createTreeMap(hierarchy);
+    let movieTiles = hierarchy.leaves();
+
+    let block = canvas.selectAll('g')
+                        .data(movieTiles)
+                        .enter()
+                        .append('g');
+    block.append('rect')
+            .attr('class', 'tile');
 }
 
 //fetching data with d3.json method
