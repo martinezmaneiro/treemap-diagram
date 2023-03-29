d426893f0581e33d1c0ffe730cc6ebe8871966d6
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-29 09:53:43 -0300

    [ADDED] svg selector variable and dataset url

---
+++ index.js
@@ -0,0 +1,7 @@
+let movieDataUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json';
+//the data extracted from the url will be stored in this variable
+let movieData;
+
+//svg element selector
+let canvas = d3.select('#canvas');
+
